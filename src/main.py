2ed5f9c168d079c5aafa1204a319238c8a4d3605
Gossepojke <svedlund.oskar@gmail.com>
2022-03-23 15:08:58 +0100

    done fore the most part

--- src/main.py
+++ src/main.py
@@ -3,12 +3,20 @@ from resorces import Quiz, Question
 
 def main():
     q1 = Question("Vilken färg är himlen?", "blå", ["blå", "röd", "grön", "gul"])
-    q2 = Question("Hur många tjejer går i INF20?", "2", ["2,", "3", "10", "5"])
+    q2 = Question("Hur många tjejer går i INF20?", "2", ["2", "3", "10", "5"])
     oskar_quiz = Quiz("Random NTI Facts" , [q1, q2])
 
-    quiz1 = Quiz("Quiz 1", 0)
-    
+
+    oskar_quiz.start_quiz()
+
+    #läs in frågor från fil!
+    # spara dessa på formen
+    # Fråga/Svar/Alternativ1,alternativ2,alternativ3
+    # osv.
+
+    # ex. Vilken färg är himlen?/blå/blå, röd, grön, gul
+    # for-loopa igenom filen med f.readlines()
+    # varje index i f.readlines är en rad (lagrad som en lista)
 
 if __name__ == "__main__":
-    main
-    
+    main()
--- src/resorces.py
+++ src/resorces.py
@@ -1,53 +1,55 @@
 
-from random import randint
+from random import shuffle
+
+
 class Question:
-    def __init__(self, question, answer, alterantives) -> None:
+    def __init__(self, question, answer, alterantives):
         self.question = question            # själva frågan
         self.answer = answer                # svaret
         self.alternatives = alterantives    # alternativen
+        shuffle(self.alternatives)
+
+    def get_answer(self):
+        return self.answer
+
+    def get_answer(self):
+        return self.answer
+
+    def get_alternatives(self):
+        return self.alternatives 
+    
 
     def ask_question(self):
+        print()
+        print(self.question)
+        for alt in self.alternatives:
+            print(alt)
         answer = input("Your answer: ")
         if answer == self.answer:
-            print("Correct!!")
+            print("Correct!!!")
+        else:
+            print("Incorrect...")
 
 class Quiz:
     def __init__(self, name, questions):
         self.name = name
         self.questions = questions
-        #self.points = 0
-        #self.list = randint(1, 2)
+        shuffle(self.questions)
 
     def get_name(self):
         return self.name
-
-    def get_list(self):
-        return self.list
-
     
-    # def generate_list(self):
-    #     random_list = randint(0, 10)
-        # if random_list == 0: self.list =  
-        # elif random_list == 1: self.list =  
-        # elif random_list == 2: self.list =  
-        # elif random_list == 3: self.list =  
-        # elif random_list == 4: self.list =  
-        # elif random_list == 5: self.list =  
-        # elif random_list == 6: self.list =  
-        # elif random_list == 7: self.list =  
-        # elif random_list == 8: self.list =  
-        # elif random_list == 9: self.list =  
-        # elif random_list == 10: self.list =  
-
+    def get_questions(self):
+        return self.questions
     
-    def generate_question(self):
-        if self.list == 1:
-            awnser = input(f"what year is it? \n1. 2014 \n2. 2020 \n3. 2022")
-            if awnser == "3" or "3." or "2022":
-                print("Rigth awnser")
-
-            else: 
-                print("wrong")
-        elif self.list == 2:
-            print("NO")
+    def start_quiz(self):
+        #input(f"{self.questions}")
+        print(f"Welcome to the {self.name} quiz!")
+        for question in self.questions:
+            question.ask_question()
     
+    def load_questions():
+        quiz = []
+        with open ("question.txt", "r", encoding="utf8") as f:
+            for line in f.readlines():
+                
